00d9ed7a67f1b027f1fe88bd3179c624713f6a80
Leonardo Slepack <111914236+LeoSlepack@users.noreply.github.com>
2022-10-17 15:52:25 -0300

    studying special numbers

--- dados/scriptnumber.js
+++ dados/scriptnumber.js
@@ -1,3 +1,22 @@
+
 console.log(typeof 5);
 console.log(typeof 12.7);
 console.log(typeof -123);
+ 
+
+
+console.log(typeof (2 + 2));
+console.log(2 * 4 - 3);
+console.log(8 / 4);
+console.log(typeof (5 + (2 * 4)));
+console.log(20 % 2);
+console.log(21 % 2);
+
+console.log(Infinity);
+console.log(-Infinity)
+console.log(NaN);
+
+
+console.log(typeof Infinity);
+console.log(typeof -Infinity)
+console.log(typeof NaN);
