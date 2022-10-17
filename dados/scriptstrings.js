7cbe0f3b03c0aa0ba7074ec1b7431d7ef356062e
Leonardo Slepack <111914236+LeoSlepack@users.noreply.github.com>
2022-10-17 16:05:22 -0300

    starting study strings

---
+++ dados/scriptstrings.js
@@ -0,0 +1,8 @@
+console.log("Strings são os textos que são digitados pelos programadores no js, e se caso colocar seu typeof vai ver que não importa quais dos três tipos de aspas vc usar, ele continua sendo string")
+console.log('isso é uma string');
+console.log("esse texto tambem é");
+console.log(`esse tambem`)
+
+console.log(typeof 'isso é uma string');
+console.log(typeof "esse texto tambem é");
+console.log(typeof `esse tambem`)
