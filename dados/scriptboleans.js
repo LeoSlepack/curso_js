142704987d65298cd43cbe68af73640db1a8eb8e
Leonardo Slepack <111914236+LeoSlepack@users.noreply.github.com>
2022-10-17 17:36:35 -0300

    training numbers, strings and booleans

--- dados/scriptboleans.js
+++ dados/scriptboleans.js
@@ -16,3 +16,23 @@ console.log(5 != 5);
 console.log(5.55  === 5.55);
 console.log(5 === 5.5);
 
+/* Exercícicos */
+
+console.log("Essa é a string");
+console.log('Essa é a segunda string');
+console.log(`Essa é a terceira string`);
+
+console.log(5);
+console.log(1.5);
+console.log(5 + 5);
+
+console.log(5 > 1);
+console.log(5 <= 4);
+console.log(5 != 5);
+
+console.log(3 == 3 && 4 > 1);
+console.log(2 == 1 || 2 < 1);
+console.log(!(6 === 2));
+
+console.log(415 * "não é número");
+
