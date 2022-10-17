5a47e37b87316c25fe94c144e22239769bdf0f15
Leonardo Slepack <111914236+LeoSlepack@users.noreply.github.com>
2022-10-17 16:54:13 -0300

    studying booleans

---
+++ dados/scriptboleans.js
@@ -0,0 +1,18 @@
+console.log(5 > 2);
+console.log(3 > 10);
+
+console.log(5 > 2);
+console.log(5 > 6);
+console.log(5 < 6);
+console.log(5 > 2);
+console.log(5 >= 4);
+console.log(5 >= 6);
+console.log(5 <= 6);
+console.log(5 >= 2);
+console.log(5 == 5);
+console.log(5 == 2);
+console.log(5 != 2);
+console.log(5 != 5);
+console.log(5.55  === 5.55);
+console.log(5 === 5.5);
+
