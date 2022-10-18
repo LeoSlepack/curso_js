88413a244c443509324b5730ae78288650d9f06f
Leonardo Slepack <111914236+LeoSlepack@users.noreply.github.com>
2022-10-18 13:41:56 -0300

    studying functions built in

---
+++ funções/scriptfun.js
@@ -0,0 +1,30 @@
+/* PROMPT */
+let idade = prompt('Qual sua idade?');
+
+console.log(idade);
+
+let nome = prompt("Qual seu nome?");
+
+console.log(nome);
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos`)
+
+/* ALERT */
+alert("Você está usando boné")
+
+/* MATH */
+let maiorNumero = Math.max(1,5,2,3);
+
+console.log(maiorNumero);
+
+let menor = Math.min(-1,05,0,4);
+
+console.log(menor);
+
+let arredondar = Math.round(4.49);
+
+console.log(arredondar);
+
+let arredondarParaCima = Math.ceil(4.99);
+
+console.log(arredondarParaCima);
+
