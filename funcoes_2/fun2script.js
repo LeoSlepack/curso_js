c0218f8646c6a712eaba2d2313cd4d84507ea321
Leonardo Slepack <111914236+LeoSlepack@users.noreply.github.com>
2022-10-18 17:33:30 -0300

    more studyes about functions .

---
+++ funcoes_2/fun2script.js
@@ -0,0 +1,91 @@
+
+function imprimirNoConsole() {
+    console.log("Hello World!");
+}
+
+imprimirNoConsole();
+
+function imprimirUmNumero(num){
+    console.log("O numero é " + num);
+}
+
+imprimirUmNumero(2);
+imprimirUmNumero(4);
+imprimirUmNumero(8);
+
+/*---------------------------------------------------------------------------------------*/
+
+const numeroAleatorio = function() {
+    console.log(Math.random());
+}
+
+numeroAleatorio();
+numeroAleatorio();
+
+/*-------------------------------------------------------------------------------------*/
+
+function multiplicarTresNumeros(x, y, z) {
+    return x * y * z;
+}
+
+console.log(multiplicarTresNumeros(2,3,4));
+
+const mult = multiplicarTresNumeros(5,4,8);
+
+console.log("O valor de mult é " + mult);
+
+/*----------------------------------------------------------------------------------------*/
+
+function podeDirigir(idade, cnh) {
+    if (idade >= 18 && cnh == true) {
+        console.log("Pode dirigir!");
+    } else {
+        console.log("Não pode dirigir!")
+    }
+}
+
+console.log(podeDirigir(19, true));
+console.log(podeDirigir(25, true));
+console.log(podeDirigir(17, false));
+console.log(podeDirigir(44,0));
+console.log(podeDirigir(19,1));
+
+/*----------------------------------------------------------------------------------------------*/
+
+let consoleTeste = () => {
+    console.log("Olá Mundo!");
+};
+
+consoleTeste();
+/*------------------------------------------------------------------------------------------------------*/
+
+let soma = (a,b) => {
+    return a + b;
+};
+
+console.log(soma(10,20));
+/*----------------------------------------------------------------------------------------------------------*/
+
+let raizQuadrada = (x) => {
+    return x * x;
+};
+
+console.log(raizQuadrada(10));
+
+/*-----------------------------------------------------------------------------------------------------------*/
+
+function mais(c, d) {
+
+    if(c === undefined || d === undefined) {
+        console.log("Esta função precisa ter os dois argumentos para ser validada.");
+    } else {
+        return c + d;
+    }
+}
+
+console.log(mais(1));
+
+console.log(mais(1,2));
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
