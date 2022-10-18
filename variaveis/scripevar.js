74f5e269e054c231a4decb2a0dc9829d487377a1
Leonardo Slepack <111914236+LeoSlepack@users.noreply.github.com>
2022-10-18 12:46:03 -0300

    start study if  variables

---
+++ variaveis/scripevar.js
@@ -0,0 +1,24 @@
+let nome = "Leonardo"
+
+console.log(nome);
+console.log(`O meu nome é ${nome}`);
+
+let laranjas = 5;
+
+console.log(laranjas * laranjas);
+
+nome = "Leticia";
+
+console.log(nome);
+
+laranjas = 2022;
+
+console.log(laranjas);
+
+laranjas = "laranja";
+
+console.log(laranjas);
+
+let um = 1, dois = 2, tres = 3;
+
+console.log(um + dois + tres);
