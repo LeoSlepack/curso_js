03174eecbefe73c86d17ce81894f62f6a330f8a5
Leonardo Slepack <111914236+LeoSlepack@users.noreply.github.com>
2022-10-18 15:36:43 -0300

    more about my study with js

--- repeticao/repscript.js
+++ repeticao/repscript.js
@@ -21,6 +21,45 @@ do {
 } while ( u < 5);
 
 /* FOR */
-for (let numero = 2; numero < 100; numero = numero *2) {
+for (let numero = 2; numero < 100; numero = numero * 2) {
     console.log("O numero é: " + numero)
 }
+
+/* BREAK */
+for(let i = 5; i < 10; i = i + 1) {
+    if( 1 % 10 == 0){
+        console.log("Saiu do loop");
+        break;
+    }
+    console.log("Prosseguindo com o loop");
+}
+
+/* CONTINUE */
+for( let t = 10; t > 0; t = t - 1) {
+
+        if( t % 2 == 0) {
+            console.log("Caiu no continue");
+            continue;
+        }
+
+        console.log(t);
+}
+
+/* INCREMENTAR A VARIAVEL */
+let n = 5;
+
+while( n < 50 ) {
+
+    console.log(n);
+
+    n += 10;
+}
+
+for(let c = 0; c < 10; c++) {
+    console.log("O c é " + c);
+}
+
+for(let c = 10; c > 0; c--) {
+    console.log("O c é " + c);
+}
+
