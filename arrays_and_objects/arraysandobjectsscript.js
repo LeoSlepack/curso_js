6e1f818bc12be29850d4bba39fb2e8a734771a0c
Leonardo Slepack <111914236+LeoSlepack@users.noreply.github.com>
2022-10-19 13:37:05 -0300

    more about arrays

--- arrays_and_objects/arraysandobjectsscript.js
+++ arrays_and_objects/arraysandobjectsscript.js
@@ -1,9 +1,16 @@
+
 let arr = [1, 4, 5, 6, 7];
 let nomes = ['Leo', 'Leticia', 'Eduardo'];
 let bool = [true, false, true];
 let misturado = ['Leo', 1, true];
-
+/*
 console.log(arr);
 console.log(nomes);
 console.log(bool);
 console.log(misturado);
+*/
+
+console.log(arr[4]);
+console.log(nomes[0]);
+console.log(bool[2]);
+console.log(misturado[misturado.length -1]);
