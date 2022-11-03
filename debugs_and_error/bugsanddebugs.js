6b2d0419ade38a6cbbf5a26449c87dd49f684745
Leonardo Slepack <111914236+LeoSlepack@users.noreply.github.com>
2022-11-03 15:42:10 -0300

    finished studies about bugs and debugs

--- debugs_and_error/bugsanddebugs.js
+++ debugs_and_error/bugsanddebugs.js
@@ -48,14 +48,14 @@ console.log(a);
 //Metodo debugger
 if(c > a) {
     a = b;
-    debugger;
+    //debugger;
 }
 
 for(let i = 5; i > 0; i--) {
     b++;
     a = a + c + 1;
     c += 2;
-    debugger;
+    //debugger;
 }
 
 if(b == a) {
@@ -64,8 +64,73 @@ if(b == a) {
     a = a + b + c;
 }
 
-debugger;
+//debugger;
 
 a = a * b;
 
-debugger;
+//debugger;
+
+//Tratamento de inputs
+/*function checarNumero(num) {
+    let number = Number(num);
+    if(Number.isNaN(number)) {
+       alert("Por favor, passe apenas números para o programa.");
+    } else {
+        return number
+    }
+}
+
+checarNumero(5);
+checarNumero('gyhwfgsyhdgfyuhsd');
+
+let number = prompt("Digite um número: ");
+
+checarNumero(number);
+
+//Exceptions
+function saudacao(nome) {
+    if(typeof nome != 'String') {
+        throw new Error("O parâmetro nome precisa ser uma sting");
+    } else {
+        console.log(`Olá ${nome}.`);
+    }
+}
+
+saudacao("Leonardo");
+saudacao(5);
+
+console.log("pausa desfeita");*/
+
+//Try and Catch and finally
+let v = 5;
+
+try {
+
+    let o = 2 + v;
+
+} catch(e) {
+
+    console.log(e);
+
+} finally {
+
+console.log("Executou")
+
+}
+
+// Assertion
+let arr = [1, 2, 3, 4, 5];
+let arr1 = [];
+
+function iterarArray(arr) {
+    if(arr.length == 0) {
+        throw new Error("O array precisa ter pelo menos um elemento")
+    } else {
+        for(let i = 0; i < arr.length; i ++) {
+            console.log(i);
+        }
+    }
+}
+
+iterarArray(arr);
+iterarArray(arr1);
