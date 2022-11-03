d50faba338a23ffc2dbd29c1dfe111d972dce90e
Leonardo Slepack <111914236+LeoSlepack@users.noreply.github.com>
2022-11-03 17:24:53 -0300

    finished studies about regex

--- expressoes_regulares/expreg.js
+++ expressoes_regulares/expreg.js
@@ -36,5 +36,68 @@ const reg4 = /[0-9]/;
 
 console.log(reg4.test("Temos os número 60?"));
 
-// Caracteres especiais
+// Regex com operador not
+const notab = /[^ab]/;
 
+console.log(notab.test("a"));
+console.log(notab.test("b"));
+console.log(notab.test("Aqui tem a e b, mas aceita pq ele so barra quando estão isolados"));
+/*---------------------------------------------------------------------------*/
+
+const nottoaz = /[^a-z]/;
+
+console.log(nottoaz.test("asd"));
+console.log(nottoaz.test("123"));
+console.log(nottoaz.test("123 asd"));
+console.log(nottoaz.test("djhgvdfvbghdgbjvdbhvbhdbvfhj"));
+
+//Regex com operador plus +
+const umOuMaisNumeros = /\d+/;
+
+console.log(umOuMaisNumeros.test("123454152352415"));
+console.log(umOuMaisNumeros.test(""));
+console.log(umOuMaisNumeros.test("kdnvjdfbjhvbfdhjbvhjfdb"));
+console.log(umOuMaisNumeros.test("111111111111"));
+
+//Regex com operador question ou "?"
+const padrao = /Abacax?i/;
+
+console.log(padrao.test("Abacai"));
+console.log(padrao.test("Abacaxi"));
+
+//Regex com operador de precisão
+const cep = /\d{5}-\d{3}/;
+
+console.log(cep.test("83730-170"));
+console.log(cep.test("83730--170"));
+console.log(cep.test("jvegwvgh"));
+console.log(cep.test("84465454"));
+
+//Regex com método exec
+let teste1 = /\d+/
+
+console.log(teste1.exec("Tem o número 100 aqui"));
+console.log(teste1.exec("Tem o número aqui"));
+
+//Choice pattern
+let frutas = /\d+ (bananas|maçãs|laranjas)/;
+
+console.log(frutas.test("10 bananas"));
+console.log(frutas.test("25 batatas"));
+console.log(frutas.test("8 laranjas"));
+
+//Regex para validar um domínio
+let validarDominio = /www.\w+\.com|com.br/;
+
+console.log(validarDominio.test("www.google.com"));
+console.log(validarDominio.test("www.teste"));
+console.log(validarDominio.test("teste.com"));
+console.log(validarDominio.test("www.octolegal.com"));
+
+//Regex validando data de nascimento
+let validarEmail = /\w+@\w+\.\w+/;
+
+console.log(validarEmail.test("teste@gmail.com"));
+console.log(validarEmail.test("teste@gmail"));
+console.log(validarEmail.test("gmail.com"));
+console.log(validarEmail.test("testwbdhjwebvfhje@gmail.com"));
