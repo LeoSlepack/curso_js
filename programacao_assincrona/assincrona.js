a0acd924de077d23df6f9709a18c8af748f4975f
Leonardo Slepack <111914236+LeoSlepack@users.noreply.github.com>
2022-11-04 13:47:03 -0300

    update js

---
+++ programacao_assincrona/assincrona.js
@@ -0,0 +1,17 @@
+// Callbacks
+console.log("1");
+
+setTimeout(function() {
+    console.log("5");
+}, 2000);
+
+console.log("2");
+console.log("3");
+console.log("4");
+
+//Promises
+let promessa = Promise.resolver(4 + 8);
+
+console.log('Algum código');
+
+promessa.then((value) => console.log('Asoma é ${value} ))
