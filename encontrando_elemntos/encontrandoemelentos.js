e61be4bec3ef1a47c2988402662db1092cbfe169
Leonardo Slepack <111914236+LeoSlepack@users.noreply.github.com>
2022-11-07 17:22:43 -0300

    studies about js

---
+++ encontrando_elemntos/encontrandoemelentos.js
@@ -0,0 +1,11 @@
+console.log(document.getElementsByTagName("h1"));
+
+console.log(document.getElementById("text "));
+
+console.log(document.getElementsByClassName("lista_exemplo"));
+
+console.log(document.querySelector("#text"));
+console.log(document.querySelector(".lista_exemplo"));
+console.log(document.querySelector("h1"));
+
+//Alterando o html
