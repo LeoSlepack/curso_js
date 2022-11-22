0fc3df1b86d9737ad061795f90e13db086e39715
Leonardo Slepack <111914236+LeoSlepack@users.noreply.github.com>
2022-11-22 16:58:15 -0300

    studies

--- teste_modal_confirm/script.js
+++ teste_modal_confirm/script.js
@@ -1,5 +1,4 @@
 const openModalButton = document.querySelector("#open-modal1");
-const closeModalButton = document.querySelector("#close-modal1");
 const modal = document.querySelector("#modal1");
 const fade = document.querySelector("#fade1");
 
@@ -8,7 +7,7 @@ const toggleModal = () => {
     [modal1,fade1].forEach((el) => el.classList.toggle("hide"));
 };
 
-/*------------------------------------*/
+
 
 
 
