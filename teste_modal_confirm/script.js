de5c9bed649a261174f14dfe0655c80afe8b25bd
Leonardo Slepack <111914236+LeoSlepack@users.noreply.github.com>
2022-11-25 14:35:21 -0300

    modal quase pronto

--- teste_modal_confirm/script.js
+++ teste_modal_confirm/script.js
@@ -3,5 +3,28 @@ const button = document.getElementById('submit-form-contact')
 button.addEventListener('click', () => {
     event.preventDefault();
 
-    alert('Botão acionado')
+    const username = document.getElementById('name0')
+    const emailConfirm = document.getElementById('email0')
+    const phoneConfirm = document.getElementById('phone_number0')
+
+    if (username.value == '' ) {
+        username.classList.add("error-input");
+    }
+
+    if (emailConfirm.value == '' ) {
+        emailConfirm.classList.add("error-input");
+    }
+
+    if (phoneConfirm.value == '' ) {
+        phoneConfirm.classList.add("error-input");
+    }
+
+    if (!this.form.checkbox.checked) {
+        checkbox.classList.add("error-input");
+    }
 });
+
+
+
+
+
